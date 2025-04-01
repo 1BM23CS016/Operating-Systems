b1f243cc7d892ef7af97fd42791a921f4cfef96d
Afreen Anz <afreenanz.cs23@bmsce.ac.in>
2025-04-01 12:09:12 +0530

    Update lab4_semaphore.c

--- codes/lab4_semaphore.c
+++ codes/lab4_semaphore.c
@@ -1,71 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 3  // Maximum buffer size
+#define BUFFER_SIZE 3
 
-int mutex = 1;  // Binary semaphore (1 = unlocked, 0 = locked)
-int full = 0;   // Number of full slots
-int empty = BUFFER_SIZE;  // Number of empty slots
-int x = 0;  // Shared resource (number of items in buffer)
+int mutex = 1;
+int full = 0;
+int empty = BUFFER_SIZE;
+int x = 0;
 
-void wait(int *S) {
-    while (*S <= 0);  // Busy wait (Not recommended for real OS)
-    (*S)--;
+void wait(int S) {
+    while (S <= 0);
+    (S)--;
 }
-
-void signal(int *S) {
-    (*S)++;
+void signal(int S) {
+    (S)++;
 }
-
 void producer() {
-    wait(&mutex);  // Lock critical section
-    wait(&empty);  // Check for available buffer space
-
+    wait(mutex);
+    wait(empty);
     x++;
     printf("Producer has produced: Item %d\n", x);
-
-    signal(&full);  // Increase full slots
-    signal(&mutex); // Unlock critical section
+    signal(full);
+    signal(mutex);
 }
 
 void consumer() {
-    wait(&mutex);  // Lock critical section
-    wait(&full);  // Check if buffer has items
-
+    wait(mutex);
+    wait(full);
     printf("Consumer has consumed: Item %d\n", x);
     x--;
-
-    signal(&empty);  // Increase empty slots
-    signal(&mutex); // Unlock critical section
+    signal(empty);
+    signal(mutex);
 }
-
 int main() {
     int choice;
-
     printf("\nEnter 1.Producer 2.Consumer 3.Exit\n");
-
     while (1) {
         printf("Enter your choice: ");
         scanf("%d", &choice);
         switch (choice) {
-            case 1:
-                if (empty > 0)
+            case 1: if (empty > 0){
                     producer();
-                else
+                    }
+                else{
                     printf("Buffer is full!\n");
-                break;
-            case 2:
-                if (full > 0)
+                    }
+                     break;
+            case 2: if (full > 0){
                     consumer();
-                else
+                    }
+                else{
                     printf("Buffer is empty!\n");
-                break;
-            case 3:
-                exit(0);
-            default:
-                printf("Invalid choice! Try again.\n");
+                    }
+                    break;
+            case 3: exit(0);
+            default: printf("Invalid choice! Try again.\n");
         }
     }
-
     return 0;
 }
+
